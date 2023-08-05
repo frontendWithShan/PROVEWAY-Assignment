8cd65fa8e8d9918e0b94321aac66c8b1fa50c7f6
Shantanu Singh <129811851+frontendWithShan@users.noreply.github.com>
2023-08-05 21:07:42 +0530

    Add files via upload

---
+++ PROVEWAY Assignment/script.js
@@ -0,0 +1,62 @@
+ 
+
+  const checkboxes = document.querySelectorAll('input[type="checkbox"][name="boxCheckbox"]');
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        
+        checkboxes.forEach((c) => {
+          const box = c.closest('.box');
+        box.style.backgroundColor = c.checked && '#F4FBF9' ; 
+          if (c !== checkbox) {
+            const box = c.closest('.box'); 
+        box.style.backgroundColor = c.checked &&  '#ffffff';
+            
+            c.checked = false;
+            hideDropdown(c);
+          }
+        });
+      }
+      showDropdown(checkbox);
+    });
+  });
+
+  function showDropdown(checkbox) {
+    const dropdown2pair = document.getElementById('dropdown2pair');
+    const dropdown3pair = document.getElementById('dropdown3pair');
+
+    if (checkbox.id === 'checkbox2') {
+      dropdown2pair.classList.toggle('hidden-dropdown', !checkbox.checked);
+    } else if (checkbox.id === 'checkbox3') {
+      dropdown3pair.classList.toggle('hidden-dropdown', !checkbox.checked);
+    }
+  }
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
+      calculateTotal();
+    });
+  });
+
+  function calculateTotal() {
+    const checkboxValues = [0.5, 0.4, 0.6]; 
+    let totalAmount = 0;
+
+    checkboxes.forEach((checkbox, index) => {
+      if (checkbox.checked) {
+        totalAmount += checkboxValues[index] * 195;
+      }
+    });
+
+    const totalAmountElement = document.getElementById('totalAmount');
+    totalAmountElement.textContent = totalAmount.toFixed(2);
+  }
+  function hideDropdown(checkbox) {
+    const dropdown2pair = document.getElementById('dropdown2pair');
+    const dropdown3pair = document.getElementById('dropdown3pair');
+
+    if (checkbox.id === 'checkbox2') {
+      dropdown2pair.classList.add('hidden-dropdown');
+    } else if (checkbox.id === 'checkbox3') {
+      dropdown3pair.classList.add('hidden-dropdown');
+    }
+  }
